7cabda247c6293f77fa93dd78ef5b964d68b4253
João Alencar <joao.alencar19982@gmail.com>
2024-02-05 22:33:02 -0300

    CQRS em products

---
+++ CleanArchMvc.Application/Products/Commands/ProductCreateCommand.cs
@@ -0,0 +1,6 @@
+﻿namespace CleanArchMvc.Application.Products.Commands
+{
+    public abstract class ProductCreateCommand : ProductCommand
+    {  
+    }
+}
---
+++ CleanArchMvc.Application/Products/Commands/ProductUpdateCommand.cs
@@ -0,0 +1,7 @@
+﻿namespace CleanArchMvc.Application.Products.Commands
+{
+    public abstract class ProductUpdateCommand : ProductCommand
+    {
+        public int Id { get; set; }
+    }
+}
